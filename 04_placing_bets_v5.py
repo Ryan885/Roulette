6c7426c8234a8327706e4d937d4bd9f31407d4fc
Ryan885 <ryan.osbaldiston@liston.school.nz>
2021-08-09 15:19:40 +1200

    Roulette Assesment

--- 04_placing_bets_v5.py
+++ 04_placing_bets_v5.py
@@ -19,7 +19,6 @@ def yes_no(question):
             print("Please answer yes / no")
         # neither
 
-
 def colourselection(question):
     error = "Please select red or black."
     valid = False
@@ -34,12 +33,8 @@ def colourselection(question):
         else:
             print(error)
 
-
-
 def paritybetfunction(money):
     global newmoney
-    if newmoney != "":
-        money = newmoney
     yes_no_parity = yes_no("Would you like to place a bet on parity?")
     if yes_no_parity == "yes":
         parity = betting("How much would you like to bet on parity?")
@@ -47,7 +42,6 @@ def paritybetfunction(money):
         money -= parity
         print("Your remaining balance is ${}.".format(money))
         newmoney = money
-    return newmoney
 
 def colourbetfunction(money):
     global newmoney
@@ -60,7 +54,6 @@ def colourbetfunction(money):
         money -= colourbet
         print("Your remaining balance is ${}.".format(money))
         newmoney = money
-        return newmoney
 
 def betting(question):
     error = "You must have enough funds for your bet & type an integer.\n"
@@ -70,7 +63,7 @@ def betting(question):
             # ask the question and remove the $ symbol if the user has entered it
             response = int(input(question).replace('$', ''))
             # if the amount is too low / too high give
-            if response <= money and response > 0:
+            if response <= newmoney and response > 0:
                 return response
             else:
                 print(error)
@@ -82,29 +75,9 @@ budgetcash = 50
 money = budgetcash
 newmoney = money
 
+print("round 1")
 paritybetfunction(money)
-print(newmoney)
 colourbetfunction(money)
-
+print("\n round 2")
 paritybetfunction(money)
-print(newmoney)
 colourbetfunction(money)
-
-
-yes_no_parity = yes_no(" you like to place a bet on parity?")
-if yes_no_parity == "yes":
-    parity = betting("How much would you like to bet on parity?")
-    print("You have placed a bet of ${}.".format(parity))
-    money -= parity
-print("Your remaining balance is ${}. \n".format(money))
-
-# round two
-
-
-yes_no_colour = yes_no("Would you like to place a bet on colour?")
-if yes_no_colour == "yes":
-    colourselection("Which colour would you like to place a bet on? (Red/Black)")
-    colourbet = betting("How much would you like to bet on colour?")
-    print("You have placed a bet of ${}.".format(colourbet))
-    money -= colourbet
-print("Your remaining balance is ${}.".format(money))
