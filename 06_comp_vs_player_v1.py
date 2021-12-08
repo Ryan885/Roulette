7814eb27d91f5e54b3f344ebc52bb5751099e946
Ryan885 <ryan.osbaldiston@liston.school.nz>
2021-12-08 16:41:09 +1300

    Roulette Assesment

--- 06_comp_vs_player_v1.py
+++ 06_comp_vs_player_v1.py
@@ -1,7 +1,11 @@
 import random
-
+parity = "odd"
+colour = "red"
+money = 50
+newmoney = money
+betting = 20
 ball_results = ["G0", "R1", "B2", "R3", "B4", "R5", "B6", "B7", "R8", "B9", "R10", "B11", "R12", "R13", "B14", "R15", "B16", "R17", "B18", "B19", "R20", "B21", "R22", "B23", "R24", "G25"]
 
 
-comp_choice = random.choice(ball_results[:-1])
+comp_choice = random.choice(ball_results)
 print("Comp Choice:", comp_choice)
